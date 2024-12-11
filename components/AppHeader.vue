a58997596fbff3392d06e22c56a677854d98302d
Daniil V. <71486704+danvitu@users.noreply.github.com>
2024-12-11 15:22:57 +0300

    update and add to app button for auth users

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,30 +1,35 @@
 <template>
   <header class="flex justify-between items-center mt-8 mb-8">
-    <NuxtLink :to="!user ? '/' : '/app'" class="text-2xl font-semibold">КПТ <span class="text-primary">Ассистент</span>
+    <NuxtLink to="/" class="text-2xl font-semibold">КПТ <span class="text-primary">Ассистент</span>
     </NuxtLink>
-    <div class="space-x-4 font-mono">
-      <NuxtLink v-if="!user" to="/login">Войти</NuxtLink>
+    <div class="flex justify-between gap-4 items-center">
+      <div class="space-x-4 font-mono">
+        <UButton v-if="!user" variant="soft">
+          <NuxtLink to="/login">Войти</NuxtLink>
+        </UButton>
+        <UButton v-else-if="route.path !== '/app'" variant="soft">
+          <NuxtLink to="/app">В приложение</NuxtLink>
+        </UButton>
+      </div>
+      <UDropdown v-if="user" :items="items" :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }"
+        :popper="{ placement: 'bottom-start' }">
+        <UAvatar src="https://avatar.iran.liara.run/public" icon="i-heroicons-photo" alt="Avatar" />
+        <template #account>
+          <div class="text-left">
+            <p>
+              Вы вошли как
+            </p>
+            <p class="truncate font-medium text-gray-900 dark:text-white">
+              {{ user.email }}
+            </p>
+          </div>
+        </template>
+        <template #item="{ item }">
+          <span class="truncate">{{ item.label }}</span>
+          <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+        </template>
+      </UDropdown>
     </div>
-    <UDropdown v-if="user" :items="items" :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }"
-      :popper="{ placement: 'bottom-start' }">
-      <UAvatar src="https://avatar.iran.liara.run/public" icon="i-heroicons-photo" alt="Avatar" />
-
-      <template #account>
-        <div class="text-left">
-          <p>
-            Вы вошли как
-          </p>
-          <p class="truncate font-medium text-gray-900 dark:text-white">
-            {{ user.email }}
-          </p>
-        </div>
-      </template>
-
-      <template #item="{ item }">
-        <span class="truncate">{{ item.label }}</span>
-        <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
-      </template>
-    </UDropdown>
   </header>
 </template>
 
@@ -32,6 +37,7 @@
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
 const { toastSuccess, toastError } = useAppToast()
+const route = useRoute();
 const items = [
   [{
     slot: 'account',
